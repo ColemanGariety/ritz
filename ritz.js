79c9a4d6a6bc94a216be720e725f0e8bc5ad23e2
Parashuram N <code@r.nparashuram.com>
2013-05-21 16:34:58 -0600

    Added /g at the end of the regex to replace all the &

--- ritz.js
+++ ritz.js
@@ -1,7 +1,7 @@
 var ritz = function(node) {
   var node = node || document.body;
   if (node.nodeType == 3  && node.data.indexOf("&") != -1 && ["BODY", "SCRIPT", "STYLE"].indexOf(node.nodeName) == -1) {
-    node.parentNode.innerHTML = node.data.replace(/&/, "<span style=\"font-family: Baskerville; font-style: italic; font-weight: 100;    \">&</span>");
+    node.parentNode.innerHTML = node.data.replace(/&/g, "<span style=\"font-family: Baskerville; font-style: italic; font-weight: 100;    \">&</span>");
   };
 
   node = node.firstChild;
